f84062e05f725dfa2c63d2c1766ca1ead015ee88
Jake Agius <158342311+FNoBaby@users.noreply.github.com>
2024-10-15 13:27:18 +0200

    first commit

---
+++ Worksheet Solutions/Worksheet2/src/worksheet2/Worksheet2.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package worksheet2;
+
+import java.util.Scanner;
+
+public class Worksheet2 {
+    public static void main(String[] args) {
+       /*
+        3. a) Project Panel
+                To keep track of classes being created
+           b) Code Editor
+                Allowing the ability to write code into the file selected in the Project Panel
+           c) Output Panel
+                Showing the programmer what the output of the code which has run is, if there are Logging statement
+        4. Project
+            A project is a collection of class files and libraries which solve a problem
+        */
+       
+       //8
+        System.out.println("Hello World");
+        
+        System.out.println("Goodbye!");
+       
+        // This will output the text into the Output Window
+        
+        //9 -> Outputs the text into the Output tab
+        
+        // 10 -> Compilation will not complete + Code editor notifies that there is an error on the line
+        
+        // 11 -> Building a project means running the .class file, Running your project would allow you to
+        //       run the .class file
+        
+        // 12 -> .class files contain the compiled code, this is only readable by the interpretor
+        //    -> .java files contain the code which is written in the code editor   
+    }
+}
